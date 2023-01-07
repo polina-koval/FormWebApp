fe4d0e092ecb5ca4e8a1d537a15c84f4dd619d1b
Polina Koval <zagadka1316@gmail.com>
2023-01-07 16:47:40 +0300

    Bugfix.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from tinydb import TinyDB
 
-from utils import predicate, type_form
+from utils import type_form, is_correct_form, convert_qs_to_dict
 
 app = FastAPI()
 db = TinyDB("db.json")
@@ -14,9 +14,10 @@ async def root():
 
 @app.post("/get_form/")
 async def get_form(request: Request):
-    body = await request.json()
-    form_from_request = type_form(body)
-    form_from_db = db.search(lambda obj: predicate(obj, form_from_request))
-    if form_from_db:
-        return form_from_db[0]["name"]
-    return form_from_request
+    body = (await request.body()).decode("utf-8")
+    data = convert_qs_to_dict(body)
+    form = type_form(data)
+    for item in db:
+        if is_correct_form(form, item):
+            return item["name"]
+    return form
--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
 import re
+from urllib.parse import parse_qs
 
 
 def type_v(v):
     re_expressions = {
-        "email": r"^\S+@\w+.\w{2,4}$",
+        "email": r"^\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b$",
         "date": r"^(\d\d\.\d\d\.\d{4}|^\d{4}-\d\d-\d\d)$",
-        "phone": r"^\+7\s*\d{3}\s*\d{3}\s*\d{2}\s*\d{2}$",
+        "phone": r"^7\s*\d{3}\s*\d{3}\s*\d{2}\s*\d{2}$",
     }
     for field_type, re_expression in re_expressions.items():
         if re.fullmatch(re_expression, v):
@@ -18,8 +19,11 @@ def type_form(d):
     return res
 
 
-def predicate(obj, requirements):
-    for k, v in requirements.items():
-        if k not in obj.keys():
-            return False
-    return True
+def convert_qs_to_dict(qs: str):
+    # Convert querystring to dict
+    dct = {k: v for k, vs in parse_qs(qs).items() for v in vs}
+    return dct
+
+
+def is_correct_form(request_body, db_item):
+    return request_body.items() <= db_item.items()
